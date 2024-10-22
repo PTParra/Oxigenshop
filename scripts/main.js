7af9dec0852c740b8aa3aab1d01d69721b9d8207
PTParra <49121638+PTParra@users.noreply.github.com>
2024-10-22 17:39:37 +0200

    validacion del formulario hecha

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,5 @@ const menuHamburguesa = new MenuHamburguesa('menu-hamburguesa', 'enlaces-header'
 const barraPorcentaje = new BarraPorcentaje('barra-porcentaje');
 
 const botonScrollUp = new BotonScrollUp('volver-arriba', 200);
+
+const validacionFormulario = new ValidacionFormulario('formulario-contacto');
