e7e05749220e4c3f8dbdcc29ba71e720535c064e
PTParra <49121638+PTParra@users.noreply.github.com>
2024-10-25 09:29:59 +0200

    codigo de selector de precios transladado a fichero propio

--- scripts/main.js
+++ scripts/main.js
@@ -14,22 +14,6 @@ const validacionFormulario = new ValidacionFormularioContacto('formulario-contac
 
 validacionFormulario.inicializar();
 
-const precios = [
-    new ConversorPrecio(0, 'precio-basic'), 
-    new ConversorPrecio(25, 'precio-professional'),
-    new ConversorPrecio(60, 'precio-premium')
-]
-
-const inputSeleccionarPrecio = document.getElementById('selector-precio');
-
-inputSeleccionarPrecio.addEventListener('input', () => {
-    const nuevaMoneda = inputSeleccionarPrecio.value;
-    console.log(nuevaMoneda);
-    precios.forEach(precio => {
-        precio.cambiarNuevoPrecio(nuevaMoneda);
-    });
-})
-
 const validacionFormularioPopup = new ValidacionFormularioPopup('formulario-popup');
 
 validacionFormularioPopup.inicializar();
